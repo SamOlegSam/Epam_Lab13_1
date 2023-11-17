76fea5f01cae794a04321d8edf7b0927c0bb4a37
Oleg <sov@transoil.gomel.by>
2023-11-17 14:45:49 +0300

    17-11-2023

---
+++ Lab13-1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("Lab13_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab13_1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("67c38e7f-0446-42de-96cf-61b60e9a5ada")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      дополнительный номер версии;
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lab13-1/WebForm1.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab13_1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string str = @"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=D:\Projects\Lab13-1\Lab13-1\Lab13-1\AppData\Monitor1.mdb";
+        private OleDbConnection oledbconnection = null;
+        private OleDbCommandBuilder oledbbuilder = null;
+        private OleDbDataAdapter oledbdataadapter = null;
+        private DataSet dataset = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadData()
+        {
+
+        }
+        
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (TextBox5.Text == "")
+            {
+                TextBox5.BorderColor = Color.Red;
+            }
+            if (TextBox6.Text == "")
+            {
+                TextBox6.BorderColor = Color.Red;
+            }
+            if (TextBox7.Text == "")
+            {
+                TextBox7.BorderColor = Color.Red;
+            }
+            if (TextBox8.Text == "")
+            {
+                TextBox8.BorderColor = Color.Red;
+            }
+            else
+            {
+                oledbconnection = new OleDbConnection(str);
+                oledbconnection.Open();
+                OleDbCommand command = new OleDbCommand($"INSERT INTO Display (Модель, Диагональ, Страна, Цена) Values (@a1, @a2, @a3, @a4)", oledbconnection);
+                command.Parameters.AddWithValue("@a1", TextBox5.Text);
+                command.Parameters.AddWithValue("@a2", TextBox6.Text);
+                command.Parameters.AddWithValue("@a3", TextBox7.Text);
+                command.Parameters.AddWithValue("@a4", Convert.ToDouble(TextBox8.Text));
+                command.ExecuteNonQuery();
+                oledbconnection.Close();
+
+                TextBox5.Text = "";
+                TextBox6.Text = "";
+                TextBox7.Text = "";
+                TextBox8.Text = "";
+                Server.TransferRequest(Request.Url.AbsolutePath, false);
+            }
+        }
+
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+    }
+}
